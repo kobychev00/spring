927da997b57547b839c71207928fe8ff112db2b9
kobychev00 <kobychev00@bk.ru>
2023-10-11 06:29:05 +0300

    11.10.2023 6:27

---
+++ src/test/java/com/example/spring1010/Spring1010ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring1010;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring1010ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
